30fc1bc3620ca7da058cdddc45b50a8e9ee3a3db
samyTiaiba <tiaiba-samy@hotmail.com>
2022-04-15 02:06:50 +0200

    video-03-create-react-app

---
+++ front/frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">shopy shopy</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
